459b55062b22c2158f84cb05b5b02ddb7a054fc6
Jordi Roca Soler <jordi@dallonses.com>
2024-09-13 19:19:29 +0200

    fix: footer tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   const year: number = new Date().getFullYear();
   return (
-    <div className="flex flex-col justify-center items-center py-10 gap-4 bg-primary text-white">
+    <footer className="flex flex-col justify-center items-center py-10 gap-4 bg-primary text-white">
       <div className="text-base">&copy; {year} By Jordi Roca</div>
-    </div>
+    </footer>
   );
 };
 
